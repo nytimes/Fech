65afd18738d485c1b834f4cc11f7986dc63226e0
Aaron Bycoffe <bycoffe@gmail.com>
2012-02-20 12:12:25 -0500

    Revert "Add support for party convention filings (F4) (Issue #17)"
    This reverts commit b310dd4613c20fb05ea933ce906a8a79e8399ea8.

--- lib/fech/fech_utils.rb
+++ lib/fech/fech_utils.rb
@@ -17,7 +17,6 @@ module FechUtils
     :f3p31 => /^f3p31/i,
     :f3ps  => /^f3ps/i,
     :f3x  => /^f3x/i,
-    :f4    => /^f4[na]/i,
     :f5    => /^f5[na]/i,
     :f56   => /^f56/i,
     :f57   => /^f57/i,
