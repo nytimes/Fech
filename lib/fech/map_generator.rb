a2604b690d787aa576b0be7bc709b39b0258f880
Derek Willis <dwillis@nytimes.com>
2015-03-28 18:53:29 -0400

    added mapping for F3Z transactions

--- lib/fech/map_generator.rb
+++ lib/fech/map_generator.rb
@@ -13,7 +13,7 @@ module Fech
     FILING_VERSIONS   = ["8.0", "7.0", "6.4", "6.3", "6.2", "6.1",
                          "5.3", "5.2", "5.1", "5.0", "3"]
     BASE_ROW_TYPES    = ["HDR", "F1", "F13", "F132", "F133", "F1M", "F1S", "F2", "F24", "F3", "F3L", "F3P", "F3P31", "F3PS",
-                         "F3S", "F3X", "F4", "F5", "F56", "F57", "F6", "F65", "F7", "F76", "F9", "F91", "F92", "F93",
+                         "F3S", "F3X", "F3Z", "F4", "F5", "F56", "F57", "F6", "F65", "F7", "F76", "F9", "F91", "F92", "F93",
                          "F94", "F99", "H1", "H2", "H3", "H4", "H5", "H6",
                          "SchA", "SchA3L", "SchB", "SchC", "SchC1", "SchC2", "SchD", "SchE", "SchF", "SchL", "TEXT"]
     ROW_TYPE_MATCHERS = {
@@ -33,6 +33,7 @@ module Fech
       "F3P31"  => FechUtils::ROW_TYPES[:f3p31],
       "F3PS"   => FechUtils::ROW_TYPES[:f3ps],
       "F3X"    => FechUtils::ROW_TYPES[:f3x],
+      "F3Z"    => FechUtils::ROW_TYPES[:f3z],
       "F4"     => FechUtils::ROW_TYPES[:f4],
       "F5"     => FechUtils::ROW_TYPES[:f5],
       "F56"    => FechUtils::ROW_TYPES[:f56],
